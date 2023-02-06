6820f1a9673089b1d4e307580570669f64bf2a3e
Alex Stelea <alex.s@rdx.works>
2023-02-06 20:08:24 +0100

    feat: rewrite connect button

---
+++ src/_types.ts
@@ -0,0 +1,50 @@
+export type Account = { label: string; address: string; appearanceId: number }
+
+export const RequestStatus = {
+  pending: 'pending',
+  success: 'success',
+  fail: 'fail',
+} as const
+
+export const RequestItemType = {
+  loginRequest: 'loginRequest',
+  dataRequest: 'dataRequest',
+  sendTransaction: 'sendTransaction',
+} as const
+
+export type RequestItemType = typeof RequestItemType
+
+export type RequestItemTypes = keyof typeof RequestItemType
+
+export type RequestStatusTypes = keyof typeof RequestStatus
+
+export type WalletRequest<
+  RequestType extends RequestItemTypes,
+  Status extends RequestStatusTypes,
+  T = {}
+> = {
+  type: RequestType
+  status: Status
+  id: string
+} & T
+
+type WalletFailRequest<RequestType extends RequestItemTypes> = WalletRequest<
+  RequestType,
+  'fail',
+  { error: string }
+>
+
+export type RequestItem =
+  | WalletRequest<'loginRequest', 'pending'>
+  | WalletRequest<'loginRequest', 'success'>
+  | WalletFailRequest<'loginRequest'>
+  | WalletRequest<'dataRequest', 'pending'>
+  | WalletRequest<'dataRequest', 'success'>
+  | WalletFailRequest<'dataRequest'>
+  | WalletRequest<'sendTransaction', 'pending'>
+  | WalletRequest<
+      'sendTransaction',
+      'success',
+      { transactionIntentHash: string }
+    >
+  | WalletFailRequest<'sendTransaction'>
---
+++ src/components/connect-button.stories.ts
@@ -0,0 +1,220 @@
+import { Story, Meta } from '@storybook/web-components'
+import { html } from 'lit-html'
+import { Account, RequestItem } from '../_types'
+import './connect-button'
+import { ConnectButton } from './connect-button'
+import './connect-button.stories.css'
+
+export default {
+  title: 'Radix/Connect button states',
+} as Meta
+
+const getConnectButton: () => ConnectButton = () =>
+  document.querySelector('radix-connect-button')!
+
+const Button = (args: any) => {
+  if (args.render)
+    return html`
+      <div class="connect-button-wrapper">
+        <radix-connect-button
+          personaLabel=${args.personaLabel}
+          dAppName="Radiswap"
+          ?loading=${args.loading}
+          ?connected=${args.connected}
+          ?connecting=${args.connecting}
+          ?showPopover=${args.showPopover}
+          ?showNotification=${args.showNotification}
+          .requestItems=${args.requestItems}
+          .accounts=${args.accounts}
+          @onCancelRequestItem=${(event: CustomEvent<{ id: string }>) => {
+            const connectButton = getConnectButton()
+            connectButton.requestItems = connectButton.requestItems.map(
+              (item: RequestItem) =>
+                item.id === event.detail.id
+                  ? { ...item, status: 'fail', error: 'canceledByUser' }
+                  : item
+            )
+            connectButton.loading = false
+            connectButton.connecting = false
+          }}
+          @onDisconnect=${() => {
+            getConnectButton().connected = false
+            getConnectButton().loading = false
+            getConnectButton().requestItems = []
+          }}
+          @onDestroy=${() => {}}
+          @onConnect=${() => {
+            getConnectButton().connected = true
+            getConnectButton().loading = false
+            getConnectButton().requestItems = [
+              {
+                id: crypto.randomUUID(),
+                type: 'loginRequest',
+                status: 'success',
+              },
+              ...getConnectButton().requestItems,
+            ]
+          }}
+          @onShowPopover=${() => {
+            getConnectButton().showNotification = false
+          }}
+        >
+        </radix-connect-button>
+      </div>
+    `
+  return ''
+}
+
+const Template: Story<Partial<any>> = (args) => Button(args)
+
+const accounts: Account[] = [
+  {
+    label: 'main',
+    address:
+      'account_tdx_b_1qlxj68pketfcx8a6wrrqyvjfzdr7caw08j22gm6d26hq3g6x5m',
+    appearanceId: 1,
+  },
+  {
+    label: 'beta',
+    address:
+      'account_tdx_b_1queslxclg3ya6tyqqgs2ase7wgst2cvpwqq96ptkqj6qaefsgy',
+    appearanceId: 2,
+  },
+]
+
+export const initial = Template.bind({})
+// More on args: https://storybook.js.org/docs/web-components/writing-stories/args
+initial.args = {
+  loading: false,
+  showPopover: false,
+  connected: false,
+  connecting: false,
+  showNotification: false,
+  requestItems: [],
+  accounts,
+  personaLabel: '',
+  render: true,
+}
+
+export const Connecting = Template.bind({})
+// More on args: https://storybook.js.org/docs/web-components/writing-stories/args
+Connecting.args = {
+  loading: true,
+  showPopover: true,
+  connected: false,
+  connecting: true,
+  showNotification: false,
+  requestItems: [
+    {
+      id: crypto.randomUUID(),
+      type: 'loginRequest',
+      status: 'pending',
+    },
+  ],
+  accounts,
+  personaLabel: 'RadMatt',
+  render: true,
+}
+
+export const connected = Template.bind({})
+// More on args: https://storybook.js.org/docs/web-components/writing-stories/args
+connected.args = {
+  loading: false,
+  showPopover: true,
+  connected: true,
+  connecting: false,
+  showNotification: false,
+  requestItems: [
+    {
+      id: crypto.randomUUID(),
+      type: 'loginRequest',
+      status: 'success',
+    },
+  ],
+  accounts,
+  personaLabel: 'RadMatt',
+  render: true,
+}
+
+export const notification = Template.bind({})
+// More on args: https://storybook.js.org/docs/web-components/writing-stories/args
+notification.args = {
+  loading: false,
+  showPopover: false,
+  connected: true,
+  connecting: false,
+  showNotification: true,
+  requestItems: [
+    {
+      id: crypto.randomUUID(),
+      type: 'sendTransaction',
+      status: 'fail',
+      error: 'rejectedByUser',
+    },
+    {
+      id: crypto.randomUUID(),
+      type: 'loginRequest',
+      status: 'success',
+    },
+  ],
+  accounts: [],
+  personaLabel: 'RadMatt',
+  render: true,
+}
+
+export const oneTimeRequest = Template.bind({})
+// More on args: https://storybook.js.org/docs/web-components/writing-stories/args
+oneTimeRequest.args = {
+  loading: true,
+  showPopover: false,
+  connected: false,
+  connecting: false,
+  showNotification: false,
+  requestItems: [
+    {
+      id: crypto.randomUUID(),
+      type: 'dataRequest',
+      status: 'pending',
+    },
+  ],
+  accounts,
+  personaLabel: '',
+  render: true,
+}
+
+export const manyRequests = Template.bind({})
+// More on args: https://storybook.js.org/docs/web-components/writing-stories/args
+manyRequests.args = {
+  loading: true,
+  showPopover: true,
+  connected: false,
+  connecting: false,
+  showNotification: false,
+  requestItems: [
+    {
+      id: crypto.randomUUID(),
+      type: 'dataRequest',
+      status: 'failed',
+      error: 'rejected',
+    },
+    {
+      id: crypto.randomUUID(),
+      type: 'sendTransaction',
+      status: 'success',
+      transactionIntentHash: '123',
+    },
+    {
+      id: crypto.randomUUID(),
+      type: 'dataRequest',
+      status: 'pending',
+    },
+    {
+      id: crypto.randomUUID(),
+      type: 'sendTransaction',
+      status: 'pending',
+    },
+  ],
+  accounts,
+  personaLabel: '',
+  render: true,
+}
