fd6a0dce638b574eab18711cefd979240b5dd559
Alex Stelea <alex.s@rdx.works>
2023-08-11 08:54:52 +0200

    style: update min height

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,3 @@
-export const BUTTON_MIN_HEIGHT = 40
+export const BUTTON_MIN_HEIGHT = 32
+export const BUTTON_COMPACT_MIN_WIDTH = 32
 export const BUTTON_MIN_WIDTH = 138
-export const BUTTON_COMPACT_MIN_WIDTH = 40
