25786bb1469556343a56b6ce14f39055035f7bdb
Alex Stelea <alex.s@rdx.works>
2023-08-11 13:51:33 +0200

    fix: format todays date

--- src/helpers/format-timestamp.ts
+++ src/helpers/format-timestamp.ts
@@ -1,12 +1,26 @@
+const isToday = (someDate: Date) => {
+  const today = new Date()
+  return (
+    someDate.getDate() == today.getDate() &&
+    someDate.getMonth() == today.getMonth() &&
+    someDate.getFullYear() == today.getFullYear()
+  )
+}
+
 export const formatTimestamp = (timestamp: number | string, divider = ' ') => {
   const date = new Date(Number(timestamp))
 
-  return `${date.getDate()} ${date.toLocaleString('en-US', {
-    month: 'short',
-  })}${divider}${date.toLocaleTimeString('en-Gb', {
+  const today = isToday(date)
+  const time = date.toLocaleTimeString('en-Gb', {
     // en-GB is causing midnight to be 00:00
     hour: 'numeric',
     minute: 'numeric',
     hour12: false,
-  })}`
+  })
+
+  if (today) return `Today${divider}${time}`
+
+  return `${date.getDate()} ${date.toLocaleString('en-US', {
+    month: 'short',
+  })}${divider}${time}`
 }
